f0c26e04b4be66c4a8cc099d46c5688f3c4ae563
juanpalacios20 <juan.palacios.casanova@correounivalle.edu.co>
2025-05-10 12:58:10 -0500

    api created

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const sequelize = require('./config/database');
-const Medicine = require('./models/medicine');
+const medicineRoutes = require('./routes/medicineRoutes');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-//app.use('/api', libroRoutes);
+app.use('/api', medicineRoutes);
 
 sequelize.sync().then(() => {
   console.log('Base de datos sincronizada');
